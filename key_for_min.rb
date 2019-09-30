12613bdbc55bd5464492dca40734481040291c0f
John Schmidley <tlg8n@outlook.com>
2019-09-30 08:19:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash.collect do |key, value|
-  key.values.min_by(&:age)
-  end
-end
-
-birthday_kids.collect do |name, age|
     if lowest_value == 0 || value < lowest_value
       lowest_value = value
       lowest_key = key
